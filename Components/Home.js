72934b7e66302e13ad75dc6b25dce6b1c867b657
BAGHRUSHI4899 <149302133+BagHrushi@users.noreply.github.com>
2025-01-09 23:44:11 +0530

    Add files via upload

---
+++ Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our application.</p>
+      {/* Add any other content for the Home page here */}
+    </div>
+  );
+}
+
+export default Home;
